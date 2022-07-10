c4f16503341f00e638fe9e3cf41f3ac426b20cd6
Yi Luk Goh <ylgoh516@gmail.com>
2022-07-10 19:02:59 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from "react-router-dom";
 
 
 function App() {
